f1304c5d1737bf49d43d3a4ee6bdca5c85299ed2
Marc Gravel <gravel.m@outlook.com>
2021-11-28 19:37:10 -0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    //route exact means path must exactly match path specified to render
+    //Routes component ensures that once a route finds a path match, it does not render anything else but that route 
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
+  //strictmode is a helper component for verifying depreciated methids, 
+  //preventing side effects, and enables checks to give warning
+
+  //BrowserRouter is the router implementation
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
