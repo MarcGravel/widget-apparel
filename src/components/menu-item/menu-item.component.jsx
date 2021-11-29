49080725666497499f1f66ba87943a44a1b68270
Marc Gravel <gravel.m@outlook.com>
2021-11-28 18:38:51 -0800

    added styling and hover effect to menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,14 +5,17 @@ import './menu-item.styles.scss'
 
 //pulling title from props instead of just declaring props
 const MenuItem = ({ title, imageUrl, size }) => (
-    //using inline style to dynamically set background image of each item
-    <div 
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }}
-        className={`${size} menu-item`}>
+    //added dynamic size to class name to be able to change size of specific menu-items according to placement
+    //using inline style to dynamically set background image of each item. image is is separate div for size hover effect so container maintains size
+    <div className={`${size} menu-item`}>
+        <div
+            className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
         <div className='content'>
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
