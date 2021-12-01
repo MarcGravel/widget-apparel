0eb0946177be27c260d655f92aef687cf909e784
Marc Gravel <gravel.m@outlook.com>
2021-11-30 20:04:32 -0800

    exact prop not required in router v6 - removed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Routes, Route } from 'react-router-dom';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
-    //route exact means path must exactly match path specified to render
     //Routes component ensures that once a route finds a path match, it does not render anything else but that route 
     <div>
       <Routes>
-        <Route exact path='/' element={<HomePage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
       </Routes>
     </div>
   );
