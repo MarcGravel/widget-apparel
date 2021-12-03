b799c20af8526bd4c50483386cdf79dac86342bd
Marc Gravel <gravel.m@outlook.com>
2021-12-02 17:45:24 -0800

    style to shop page preview components

--- src/components/preview/preview.component.jsx
+++ src/components/preview/preview.component.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const Preview = ({ title, items }) => (
     <div className='preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='section-preview'>
             {
-                items.map((item) => (
-                    <div key={item.id}>{item.name}</div>
-                ))
+                //filter item list to only show 4 - array index < 4
+                //map items to display filtered array
+                items
+                    .filter((item, index) => index < 4)
+                    .map(({id, ...otherItemProps}) => (
+                        <CollectionItem key={id} {...otherItemProps} />
+                    ))
             }
         </div>
     </div>
--- src/pages/hatspage/hatspage.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HatsPage = () => (
-    <div id='hatspage'>
-        <h1>HatsPage Success</h1>
-    </div>
-);
-
-export default HatsPage;
