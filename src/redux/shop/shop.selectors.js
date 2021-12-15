e496c2d3e5774f0cd8b93285f38a11cfed76cb8d
Marc Gravel <gravel.m@outlook.com>
2021-12-14 20:10:09 -0800

    data normalization to shop_data -- array to object

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,24 +1,15 @@
 import { createSelector } from "reselect";
 
-//maps string value os url param to respective id for dynamic routing
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    shoes: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
     [selectShop],
     shop => shop.collections
-)
+);
 
 //finding the collection id matching the url paramenter of collection id map
 export const selectCollection = collectionUrlParam => 
     createSelector (
         [selectShopCollections],
-        collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-    )
+        collections => collections[collectionUrlParam]
+    );
