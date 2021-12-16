3b663f1ebecb410c98d1825032f9f2d250b612d3
Marc Gravel <gravel.m@outlook.com>
2021-12-15 17:51:53 -0800

    fixed collections-overview mapping due to data normalization and mapping data

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import './collections-overview.styles.scss';
 import { connect } from 'react-redux';
-import { selectShopCollections } from  '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from  '../../redux/shop/shop.selectors';
 import Preview from '../preview/preview.component';
 
-const CollectionsOverview = ({ collections }) => (
+const CollectionsOverview = ({ collections }) => {
+    return (
     <div className='collections-overview'>
-        {
-        collections.map(({id, ...otherCollectionProps}) => (
-            <Preview key={id} {...otherCollectionProps} />
-        ))
-    }
+        {    
+            collections.map(({id, ...otherCollectionProps}) => (
+                <Preview key={id} {...otherCollectionProps} />
+            ))
+        }
     </div>
-)
+)}
 
 const mapStateToProps = (state) => ({
-    collections: selectShopCollections(state)
+    collections: selectCollectionsForPreview(state)
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,13 @@ export const selectShopCollections = createSelector(
     shop => shop.collections
 );
 
+//returns object as array format for collections so collections-overview can map through collections
+export const selectCollectionsForPreview = createSelector(
+    [selectShopCollections],
+    //get all keys, then map over array of keys to get value of collections object at that key
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
 //finding the collection id matching the url paramenter of collection id map
 export const selectCollection = collectionUrlParam => 
     createSelector (
