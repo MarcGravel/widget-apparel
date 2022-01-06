0d580c59e9d4842abfad8acb2f5d578737aa358b
Marc Gravel <gravel.m@outlook.com>
2022-01-05 17:55:02 -0800

    checking footer palcement on pages

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,7 +3,7 @@ import './footer.styles.scss';
 
 const Footer = () => (
     <div className='footer'>
-        <h3>This is not a real e-store. You cannot buy these products. This is a personal showcase project</h3>
+        <h3>This is not a real e-store. You cannot buy these products. This is a personal showcase project.</h3>
     </div>
 )
 
